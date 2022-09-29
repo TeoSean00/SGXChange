97495e5dd5c8a6f892c0db6222bf6ca730d8b06a
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2022-09-29 09:56:00 +0800

    1st attempt at lightmode/darkmode

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,19 +1,7 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <!-- <a class="navbar-brand" href="#">Navbar</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button> -->
 
-    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+  <nav class="navbar navbar-expand navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto ">
         <li class="nav-item active">
           <router-link to="/" class="nav-link">
@@ -35,52 +23,20 @@
             ModuleMappingPage
           </router-link>
         </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li> -->
       </ul>
-      <!-- <form class="form-inline my-2 my-lg-0">
-        <input
-          class="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Search
-        </button>
-      </form> -->
     </div>
+    <button @click="colorMode = colorMode === 'dark' ? 'light' : 'dark'">
+    colorMode {{ colorMode }}
+    </button>
   </nav>
 </template>
 
 <script>
+import { useColorMode } from '@vueuse/core';
 export default {
   data() {
     return {
-
+        colorMode: useColorMode()
     };
   },
   methods: {
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from "./router/index";
 import './index.css'
+import { useColorMode } from '@vueuse/core';
 import "bootstrap/dist/css/bootstrap.css"
 
 const app = createApp(App).use(router);
+app.use(useColorMode)
 app.mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js"
