e82e4f9982ebec7d30ed2e9e087224523ea608f4
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2022-09-29 09:57:24 +0800

    deleted unnessary stuff

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from "./router/index";
 import './index.css'
-import { useColorMode } from '@vueuse/core';
 import "bootstrap/dist/css/bootstrap.css"
 
 const app = createApp(App).use(router);
-app.use(useColorMode)
 app.mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js"
