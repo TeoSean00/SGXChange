379c7d93d80994e8cde50c6530d4cbfdf55161a9
KeithLoh <keithloh99@gmail.com>
2022-10-10 16:50:51 +0800

    Fixed testFireBase code

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
-
 <template>
   <div>
-    <NavBar/>
-    <router-view ></router-view>
-    <About/>
+    <NavBar />
+    <router-view></router-view>
+    <About />
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
 import About from "./components/About.vue";
+import { ref, onMounted } from "vue";
+// import crudTest from './composables/firebase/crudTest.vue';
+const el = ref();
 
 export default {
   name: "App",
   components: {
     NavBar,
     About,
-},
+  },
 };
 </script>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,28 +1,37 @@
 <template>
-    <div id="about">
-        About Information
-    </div>
+  <div id="about">About Information</div>
 </template>
 
-<script>
-    import db from './Firebase/firebaseInit'
-    import { collection, getDocs, doc, getDoc } from "firebase/firestore";
+<script setup>
+import db from "../service/Firebase/firebaseInit";
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
+import { ref, onMounted } from "vue";
 
-    // to get all documents in a collection
-    const querySnapshot = await getDocs(collection(db, "Users"));
-    querySnapshot.forEach((doc) => {
+onMounted(async () => {
+  console.log("mounted");
+  // to get all documents in a collection
+  const querySnapshot = await getDocs(collection(db, "Users"));
+  querySnapshot.forEach((doc) => {
     // doc.data() is never undefined for query doc snapshots
-    console.log("Users collection's document id of " + doc.id, "contains ", doc.data());
-    });
+    console.log(
+      "Users collection's document id of " + doc.id,
+      "contains ",
+      doc.data()
+    );
+  });
 
-    // to get just 1 document from a specific collection
-    const docRef = doc(db, "Users", "q1dFCkSblms3sLqaECDJ");
-    const docSnap = await getDoc(docRef);
+  // to get just 1 document from a specific collection
+  const docRef = doc(db, "Users", "q1dFCkSblms3sLqaECDJ");
+  const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-    console.log("This should show data from document id: q1dFCkSblms3sLqaECDJ", docSnap.data());
-    } else {
+  if (docSnap.exists()) {
+    console.log(
+      "This should show data from document id: q1dFCkSblms3sLqaECDJ",
+      docSnap.data()
+    );
+  } else {
     // doc.data() will be undefined in this case
     console.log("No such document!");
-    }
+  }
+});
 </script>
