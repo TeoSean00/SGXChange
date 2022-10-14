cd24fa9f8587658ebbcb96b385533acb3ce9371b
KeithLoh <keithloh99@gmail.com>
2022-10-14 15:45:00 +0800

    Added Marquee Carous

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,46 +1,43 @@
-
 <template>
-    <!-- <div id="searchbar">
+  <!-- <div id="searchbar">
         <Search/>
     </div> -->
-    <div id="carousel">
+  <!-- <div id="carousel">
         <Carousel/>
+    </div> -->
+  <div>
+    <Marquee />
+  </div>
+  <div class="container-fluid p-4">
+    <div class="row">
+      <h2>Suggested universities</h2>
     </div>
-    <div class="container-fluid p-4">
-        <div class="row">
-            <h2>
-                Suggested universities
-            </h2>
-        </div>
-        <div class="row d-flex justify-content-start">
-            <UniCardSmall
-            class="unicard"
-            v-for="i in 9"
-            :key="i"
-            />
-        </div>
+    <div class="row d-flex justify-content-start">
+      <UniCardSmall class="unicard" v-for="i in 9" :key="i" />
     </div>
+  </div>
 </template>
-<script >
+<script>
 import UniCardSmall from "@/components/UniCardSmall.vue";
 // import Search from "@/components/Search.vue";
-import Carousel from "@/components/Carousel.vue";
+// import Carousel from "@/components/Carousel.vue";
+import Marquee from "@/components/Marquee.vue";
 
 export default {
   name: "HomePage",
   data() {
-    return {
-    };
+    return {};
   },
   components: {
     UniCardSmall,
     // Search,
-    Carousel,
-},
-}
+    // Carousel,
+    Marquee,
+  },
+};
 </script>
 <style scoped>
 #carousel {
-    position: relative;
+  position: relative;
 }
 </style>
