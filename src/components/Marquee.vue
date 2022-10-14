ad549ac3e811126c835a296e9d55c80d7eaf0e6f
KeithLoh <keithloh99@gmail.com>
2022-10-14 16:57:51 +0800

    Marque pause on img hover

--- src/components/Marquee.vue
+++ src/components/Marquee.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="marquee-wrapper">
     <div class="marquee">
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
-      <img class="marquee-img" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
+      <img class="marquee-img rounded" src="https://placeimg.com/235/350/any" />
     </div>
   </div>
 </template>
