4e5ee9ac03dc42e68620445d1cf350aa842fc75c
KeithLoh <keithloh99@gmail.com>
2022-10-16 13:24:31 +0800

    Fix Fontsource

--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router/index";
 
-import "@fontsource/Montserrat/100.css";
-import "@fontsource/Montserrat/200.css";
-import "@fontsource/Montserrat/300.css";
-import "@fontsource/Montserrat/400.css";
-import "@fontsource/Montserrat/500.css";
-import "@fontsource/Montserrat/600.css";
-import "@fontsource/Montserrat/700.css";
-import "@fontsource/Montserrat/800.css";
-import "@fontsource/Montserrat/900.css";
+// import "@fontsource/Montserrat/100.css";
+import "@fontsource/montserrat/100.css";
+import "@fontsource/montserrat/200.css";
+import "@fontsource/montserrat/300.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/800.css";
+import "@fontsource/montserrat/900.css";
+
 import "vue3-marquee/dist/style.css";
 import "./index.css";
 
