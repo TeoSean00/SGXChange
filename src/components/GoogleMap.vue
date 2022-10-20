33923939dee50166778a3dbe1745d1fac4bb2fef
KeithLoh <keithloh99@gmail.com>
2022-10-20 08:28:18 +0800

    Initial googlemap commit

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,20 @@
+<template>
+  <GMapMap
+    :center="{ lat: 51.093048, lng: 6.84212 }"
+    :zoom="7"
+    map-type-id="terrain"
+    style="width: 100vw; height: 900px"
+  >
+  </GMapMap>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      center: { lat: 51.093048, lng: 6.84212 },
+    };
+  },
+};
+</script>
