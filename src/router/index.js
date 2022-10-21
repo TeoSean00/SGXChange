f6b342175248629b00fd527e2cca9ab6425e121f
KeithLoh <keithloh99@gmail.com>
2022-10-21 14:07:21 +0800

    Fixed Routing connections to use proper import

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,53 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from '@/views/HomePage.vue'
-import UniversityPage from '@/views/UniversityPage.vue'
-import ModulePage from '@/views/ModulePage.vue'
-import ModuleMappingPage from '@/views/ModuleMappingPage.vue'
+import HomePage from "@/views/HomePage.vue";
+import UniversityPage from "@/views/UniversityPage.vue";
+import ModulePage from "@/views/ModulePage.vue";
+import ModuleMappingPage from "@/views/ModuleMappingPage.vue";
+import Register from "@/views/Register.vue";
+import SignIn from "@/views/SignIn.vue";
+import ProfilePage from "@/views/ProfilePage.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-      {
-        path: "/",
-        name: "HomePage",
-        component: HomePage,
-      },
-      {
-        path: "/UniversityPage",
-        name: "UniversityPage",
-        component: UniversityPage,
-      },
-      {
-        path: "/ModulePage",
-        name: "ModulePage",
-        component: ModulePage,
-      },
-      {
-        path: "/ModuleMappingPage",
-        name: "ModuleMappingPage",
-        component: ModuleMappingPage,
-      },
-      {
-        path: "/RegisterPage",
-        name: "RegisterPage",
-        component: () => import('../views/Register.vue')
-      },
-      {
-        path: "/SignInPage",
-        name: "SignInPage",
-        component: () => import('../views/Signin.vue')
-      },
-      {
-        path: "/ProfilePage",
-        name: "ProfilePage",
-        component: () => import('../views/ProfilePage.vue')
-      }
-    ],
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "HomePage",
+      component: HomePage,
+    },
+    {
+      path: "/UniversityPage",
+      name: "UniversityPage",
+      component: UniversityPage,
+    },
+    {
+      path: "/ModulePage",
+      name: "ModulePage",
+      component: ModulePage,
+    },
+    {
+      path: "/ModuleMappingPage",
+      name: "ModuleMappingPage",
+      component: ModuleMappingPage,
+    },
+    {
+      path: "/RegisterPage",
+      name: "RegisterPage",
+      component: Register,
+    },
+    {
+      path: "/SignInPage",
+      name: "SignInPage",
+      component: SignIn,
+    },
+    {
+      path: "/ProfilePage",
+      name: "ProfilePage",
+      component: ProfilePage,
+    },
+  ],
 });
-  
+
 // router.beforeEach((to, from, next) => {
 //   if (to.matched.some((record) => record.meta.requiresAuth)) {
 //     if (getAuth().currentUser){
