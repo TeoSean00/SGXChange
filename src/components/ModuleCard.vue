d5465775249434e0902dd464671bdac78ee11748
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2022-10-21 20:35:29 +0800

    moduele mapping update

---
+++ src/components/ModuleCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div :class="{box: true, selected: select}" v-on:click="selectModule">
+        {{ name }}
+    </div>
+</template>
+<script>
+export default {
+  name: "ModuleCard",
+  props: ['name'],
+  data() {
+    return {
+      select: false
+    };
+
+  },
+  methods: {
+    selectModule(){
+        this.select != this.select
+
+    }
+  }
+};
+</script>
+<style scoped>
+.box{
+    height: 200px;
+    width: 200px;
+    /* background-color: lightblue; */
+    border-radius: 10%;
+    padding: 10px;
+    margin: 10px;
+}
+.selected{
+    background-color: grey;
+}
+</style>
--- src/views/ModuleMappingPage.vue
+++ src/views/ModuleMappingPage.vue
@@ -1,3 +1,75 @@
 <template>
-    Module Mapping Page
+  Module Mapping Page
+  <div class="container">
+    <div>
+      <!-- Step one -->
+      <div class="mb-3">
+        <label for="uniInput" class="form-label">Which university are you from?</label>
+        <select
+          class="form-control"
+          id="uniInput"
+        >
+          <option v-for="uni in universities" :key="uni">{{ uni }}</option>
+        </select>
+      </div>
+      <div class="mb-3">
+        <label for="degreeInput" class="form-label">Which Degree are you taking?</label>
+          <select
+            class="form-control"
+            id="degreeInput"
+          >
+            <option v-for="degree in degrees" :key="degree">{{ degree }}</option>
+          </select>
+
+      </div>
+      <div class="mb-3">
+        <label for="secondDegreeInput" class="form-label">Do you have a second Degree?</label>
+          <select
+            class="form-control"
+            id="secondDegreeInput"
+          >
+            <option selected>Optional</option>
+            <option v-for="degree in degrees" :key="degree">{{ degree }}</option>
+          </select>
+      </div>
+      <button v-on:click="showForm" class="btn btn-primary">Submit</button>
+    </div>
+
+    <!-- Step two -->
+    <div class="form2" id="form2">
+      <div class="displayModules">
+        <ModuleCard v-for="basket in baskets" :key="basket" :name="basket"/>
+      </div>
+      <button class="btn btn-primary">Submit</button>
+    </div>
+  </div>
 </template>
+<script>
+import ModuleCard from "@/components/ModuleCard.vue";
+export default {
+    data() {
+        return {
+          universities: ['SMU', 'NUS', 'NTU'],
+          degrees: ['IS', 'CS', 'Law', 'Finance', 'Accountancy'],
+          baskets: ['Managing', 'Tech & Society', 'Cultures', 'IS Major Core', 'IS Depth Electives', 'Civilisations', 'Global Exposure'],
+          selectedBaskets: []
+        };
+    },
+    components:{
+      ModuleCard
+    },
+    methods: {
+            showForm() {
+                document.getElementById('form2').classList.remove('form2')
+            }
+        },
+    }
+</script>
+<style scoped>
+.form2 {
+    display: none;
+}
+.displayModules{
+  display: flex;
+}
+</style>
