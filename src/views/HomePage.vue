cc4823d7f0819cba09b75d5a9a1563e04984ec7b
elton <eltontay1@mail.com>
2022-10-23 01:00:08 +0800

    University Info Page skeleton added, some minor tweaks on homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,36 +7,26 @@
     </div> -->
   <Banner></Banner>
   <div class="button-banner d-flex align-center justify-content-center">
-    <div class="d-flex justify-content-center align-items-center p-5 mt-5">
+    <div class="d-flex justify-content-center align-items-center p-5">
       <router-link to="/ModuleMappingPage" class="btn btn-dark m-5 btn-lg ">Get help for module mapping</router-link>
       <span class="h3">or</span>
       <a class="btn btn-light m-5 btn-lg" href="#marquee">Browse Universities</a>
     </div>
   </div>
-  <div id="marquee">
+  <ArrowAnimation></ArrowAnimation>
+  <div id="marquee" class="container-fluid px-3">
     <Marquee />
   </div>
   <div>
     <!-- <GoogleMap /> -->
   </div>
-
-  <div class="container-fluid p-4">
-    <div class="row">
-      <h2>Suggested universities</h2>
-    </div>
-    <div class="row d-flex justify-content-start">
-      <UniCardSmall class="unicard" v-for="i in 9" :key="i" />
-      <!-- some binding needed for unicard data, need to wait for backend -->
-    </div>
-  </div>
-
   <div>
     <GoogleMap></GoogleMap>
   </div>
 </template>
 <script>
 import Banner from "@/components/Banner.vue";
-import UniCardSmall from "@/components/UniCardSmall.vue";
+import ArrowAnimation from "@/components/ArrowAnimation.vue"
 // import Search from "@/components/Search.vue";
 // import Carousel from "@/components/Carousel.vue";
 import { reactive } from "vue";
@@ -50,7 +40,7 @@ export default {
   },
   components: {
     Banner,
-    UniCardSmall,
+    ArrowAnimation,
     // Search,
     // Carousel,
     Marquee,
@@ -65,4 +55,5 @@ export default {
 .button-banner {
   height: 75vh;
 }
+
 </style>
--- src/views/UniversityPage.vue
+++ src/views/UniversityPage.vue
@@ -1,7 +1,97 @@
 <template>
+    <!-- Suggested Uni Section -->
+<div class="container-fluid mt-5">
+    <div class="row">
+        <div class="col-1"></div>
+        <div class="col">
+            <h2>Suggested universities</h2>
+        </div>
+        <div class="col-1"></div>
+    </div>
+    <!-- blank space -->
+    <div class="row">
+        <div class="col-1"></div>
+            <UniCardSmall class="col unicard" v-for="i in 4" :key="i" />
+        <div class="col-1"></div>
+    <!-- some binding needed for unicard data, need to wait for backend -->
+    </div>
+</div>
+  <!-- All Uni list section -->
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="container-fluid mt-5">
+    <div class="row">
+        <div class="col-1"></div>
+        <div class="col"><h2>All universities</h2></div>
+        <div class="col-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-1"></div>
+            <UniCardSmall class="col unicard" v-for="i in 4" :key="i" />
+        <div class="col-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-1"></div>
+            <UniCardSmall class="col unicard" v-for="i in 4" :key="i" />
+        <div class="col-1"></div>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<!-- pagination -->
+<nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center">
+    <li class="page-item disabled">
+      <a class="page-link">Previous</a>
+    </li>
+    <!-- this part should be v-for based on no. of items in data -->
+    <!-- they should be buttons that bind to v-model currentPage-->
+    <!-- paginated items should change as well , use array.slice(start,end)-->
+    <!-- create a new component for paginated items  -->
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">Next</a>
+    </li>
+  </ul>
+</nav>
+<br>
+<br>
+<br>
 
 </template>
 
 <script>
+import UniCardSmall from "@/components/UniCardSmall.vue";
 
+export default {
+  name: "UniversityPage",
+  data() {
+    return {
+        // items per page set to default
+        perPage: 3,
+        // this will be v-modelled to change according to what user clicks
+        currentPage: 1,
+        items: [
+        ]
+    };
+  },
+  computed: {
+      rows() {
+        return this.items.length
+      }
+  }, 
+  components: {
+    UniCardSmall
+  },
+};
 </script>
