16fb85cf6a7f867213f12a22b0e41bbe44b056d5
KeithLoh <keithloh99@gmail.com>
2022-11-06 19:04:14 +0800

    Revert "Changed the module card from number to select or unselected"
    This reverts commit a4d2c72fab00c015ce4222f21f8de9ecd07e5d10.

--- src/components/ModuleCard.vue
+++ src/components/ModuleCard.vue
@@ -1,72 +1,74 @@
 <template>
-  <div
-    :class="{ box: true, selected: select, unselected: !select }"
-    @click="choseMod()"
-  >
-    <h4 :class="{ selected: select, unselected: !select }">{{ name }}</h4>
-    <!-- <span class="moduleCounter">
-      <i class="fa-solid fa-minus" v-on:click="minusCount"></i>
-      {{ moduleSelect }}
-      <i class="fa-solid fa-plus" v-on:click="addCount"></i>
-    </span> -->
-  </div>
+    <div :class="{box: true, selected: select, unselected: !select}">
+      <h4 :class="{selected: select, unselected: !select}">{{ name }}</h4>
+      <span class="moduleCounter">
+        <i class="fa-solid fa-minus" v-on:click="minusCount"></i>
+        {{ moduleCount }}
+        <i class="fa-solid fa-plus" v-on:click="addCount"></i>
+      </span>
+    </div>
+
 </template>
 <script>
 export default {
   name: "ModuleCard",
-  props: ["name"],
-  emits: ["addBasket", "removeBasket"],
+  props: ['name'],
+  emits: ['addBasket', 'removeBasket'],
   data() {
     return {
       select: false,
-      moduleSelect: false,
+      moduleCount: 0,
     };
+
   },
   methods: {
-    selectModule() {
-      if (this.moduleSelect) {
-        this.select = true;
-      } else {
-        this.select = false;
+    selectModule(){
+      if(this.moduleCount > 0){
+        this.select = true
+      }
+      else{
+        this.select = false
       }
     },
-    choseMod() {
-      if (this.moduleSelect) {
-        this.moduleSelect = false;
-        this.selectModule();
-        this.$emit("removeBasket", this.name);
-      } else {
-        this.moduleSelect = true;
-        this.selectModule();
-        this.$emit("addBasket", this.name);
-      }
+    addCount() {
+      this.moduleCount++
+      this.selectModule()
+      this.$emit('addBasket', this.name)
     },
-  },
+    minusCount(){
+      if (this.moduleCount>0){
+        this.moduleCount--
+        this.selectModule()
+        this.$emit('removeBasket', this.name)
+      }
+    }
+  }
 };
 </script>
 <style scoped>
-.box {
-  height: 200px;
-  width: 200px;
-  border-radius: 10%;
-  padding: 10px;
-  margin: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
+.box{
+    height: 200px;
+    width: 200px;
+    border-radius: 10%;
+    padding: 10px;
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    text-align: center;
 
-.moduleCounter {
+}
+.moduleCounter{
   font-size: 20px;
   text-align: center;
 }
 
-.unselected {
+.unselected{
   background-color: lightblue;
 }
-.selected {
-  background-color: rgb(64, 64, 234);
-  color: white;
+.selected{
+    background-color: rgb(64, 64, 234);
+    color: white;
 }
+
 </style>
