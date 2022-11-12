9736985ac23c8a5bf98263335402bb424918df1b
KeithLoh <keithloh99@gmail.com>
2022-11-13 03:17:38 +0800

    Restyled the homepage still need to adjust fonts tmr

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -16,9 +16,157 @@
     </div>
   </div>
   <ArrowAnimation></ArrowAnimation>
-  <!-- Add Parallax Scrolling here -->
-  <div class="container-fluid textbox">
-    <!-- <img class="mx-auto d-block logo" src="../assets/WebsiteLogo.png" alt="" /> -->
+
+  <!-- NEW COMPONENT -->
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css"
+    integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA="
+    crossorigin="anonymous"
+  />
+  <div class="container mt-3 mb-3">
+    <div class="row align-items-center">
+      <div
+        class="col-lg-6 col-md-6 order-2 order-md-1 mt-4 pt-2 mt-sm-0 opt-sm-0"
+      >
+        <div class="row align-items-center">
+          <div class="col-lg-6 col-md-6 col-6">
+            <div class="row">
+              <div class="col-lg-12 col-md-12 mt-4 pt-2">
+                <div
+                  class="
+                    card
+                    work-desk
+                    rounded
+                    border-0
+                    shadow-lg
+                    overflow-hidden
+                  "
+                >
+                  <img
+                    src="../assets/friends.jpeg"
+                    class="img-fluid"
+                    alt="Image"
+                  />
+                  <div class="img-overlay bg-dark"></div>
+                </div>
+              </div>
+              <!--end col-->
+            </div>
+            <!--end row-->
+          </div>
+          <!--end col-->
+
+          <div class="col-lg-6 col-md-6 col-6">
+            <div class="row">
+              <div class="col-lg-12 col-md-12">
+                <div
+                  class="
+                    card
+                    work-desk
+                    rounded
+                    border-0
+                    shadow-lg
+                    overflow-hidden
+                  "
+                >
+                  <img
+                    src="../assets/studentTravel.jpg"
+                    class="img-fluid"
+                    alt="Image"
+                  />
+                  <div class="img-overlay bg-dark"></div>
+                </div>
+              </div>
+              <!--end col-->
+
+              <div class="col-lg-12 col-md-12 mt-4 pt-2">
+                <div
+                  class="
+                    card
+                    work-desk
+                    rounded
+                    border-0
+                    shadow-lg
+                    overflow-hidden
+                  "
+                >
+                  <img
+                    src="../assets/studentsHappy.jpg"
+                    class="img-fluid"
+                    alt="Image"
+                  />
+                  <div class="img-overlay bg-dark"></div>
+                </div>
+              </div>
+              <!--end col-->
+            </div>
+            <!--end row-->
+          </div>
+          <!--end col-->
+        </div>
+        <!--end row-->
+      </div>
+      <!--end col-->
+
+      <div class="col-lg-6 col-md-6 col-12 order-1 order-md-2">
+        <div class="section-title ml-lg-5">
+          <h5 class="text-custom mb-3 aboutUs">WHAT WE DO</h5>
+          <h4 class="title mb-4">
+            Our mission is to simplify<br />
+            the exchange process.
+          </h4>
+          <p class="text-muted mb-0">
+            As students ourselves, we understand how painful it is to
+            <br />collate information with it being scattered everywhere.<br />
+            Thus, with SGXchange your one-stop portal information is
+            consolidated and easily accessible.
+          </p>
+
+          <div class="row" style="color: #096dd9">
+            <div class="col-lg-6 mt-4 pt-2">
+              <div class="media align-items-center rounded shadow p-3">
+                <i class="fa fa-circle-info h4 mb-0 text-custom"></i>
+                <h6 class="ml-3 mb-0">
+                  <a style="color: #096dd9">One-stop Portal</a>
+                </h6>
+              </div>
+            </div>
+            <div class="col-lg-6 mt-4 pt-2">
+              <div class="media align-items-center rounded shadow p-3">
+                <i class="fa fa-file-download h4 mb-0 text-custom"></i>
+                <h6 class="ml-3 mb-0">
+                  <a style="color: #096dd9">Module Mapping</a>
+                </h6>
+              </div>
+            </div>
+            <div class="col-lg-6 mt-4 pt-2">
+              <div class="media align-items-center rounded shadow p-3">
+                <i class="fa fa-users h4 mb-0 text-custom"></i>
+                <h6 class="ml-3 mb-0">
+                  <a style="color: #096dd9">Community</a>
+                </h6>
+              </div>
+            </div>
+            <div class="col-lg-6 mt-4 pt-2">
+              <div class="media align-items-center rounded shadow p-3">
+                <i class="fa fa-face-smile-beam h4 mb-0 text-custom"></i>
+                <h6 class="ml-3 mb-0">
+                  <a style="color: #096dd9">User Friendly</a>
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--end col-->
+    </div>
+    <!--enr row-->
+  </div>
+
+  <!-- TRYING SOMETHING NEW -->
+  <!-- BELOW IS THE OLD WHY SGXCHANGE -->
+  <!-- <div class="container-fluid textbox">
     <div class="row">
       <div
         class="
@@ -65,7 +213,8 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
+  <!-- ENDS HERE -->
   <div>
     <p
       class="d-flex align-items-center justify-content-center fw-bold"
