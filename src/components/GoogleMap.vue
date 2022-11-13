b2d4ff143a65dca05704b0a050cdf37a42234556
elton <eltontay1@mail.com>
2022-11-13 17:20:39 +0800

    included tyler's commits for reordering of gmap

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mapbox overflow-auto my-4">
     <div class="row">
-      <div class="col-12 col-xl-4 navigation-panel overflow-auto" style="height:50vh">
+      <div class="col-12 col-xl-4 navigation-panel overflow-auto  order-1 order-xl-0" style="height:50vh">
         <h6 class="d-flex justify-content-between align-items-center mb-3">
           <span class="text-uppercase">Click on nearby attractions to explore</span>
           <!-- <span>{{ uniName }} Map</span> -->
